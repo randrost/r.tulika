177ba3a948158c94fb7863fd3a702fd40a2ddca5
rosti <rostik.tulika@gmail.com>
2025-08-03 15:29:53 +0200

    update: upgrade Angular dependencies to version 18 and adjust configuration settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContainerModule } from './ui/container/container.module';
 import { SharedModule } from './ui/@shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ApiModule } from './api/api.module';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    SharedModule,
-    ContainerModule,
-    ApiModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        SharedModule,
+        ContainerModule,
+        ApiModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
