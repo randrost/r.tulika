7c7cf80e506af2e17237fe911c99aa65a0fc25a7
rosti <rostik.tulika@gmail.com>
2025-08-05 23:24:16 +0200

    refactor: reworked module based structure to standalone and removed unused components

--- src/app/ui/widgets/widget-waterfall/widget-waterfall.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WidgetWaterfallDirective } from './widget-waterfall.directive';
-
-describe('WidgetWaterfallDirective', () => {
-  it('should create an instance', () => {
-    const directive = new WidgetWaterfallDirective();
-    expect(directive).toBeTruthy();
-  });
-});
