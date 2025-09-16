7eb9c8423a40d015756ac1fa29c7f6c0028bbff7
rosti <rostik.tuika@gmail.com>
2025-09-16 19:43:41 +0200

    Enhance router configuration to restore scroll position on navigation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,7 @@
 import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import {provideRouter, withInMemoryScrolling} from '@angular/router';
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
-import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
 import {provideHttpClient} from "@angular/common/http";
 import {provideTranslateService} from "@ngx-translate/core";
 import {provideTranslateHttpLoader} from "@ngx-translate/http-loader";
@@ -12,7 +11,10 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({eventCoalescing: true}),
-    provideRouter(routes),
+    // scrollt beim Navigieren nach oben
+    provideRouter(routes, withInMemoryScrolling({
+      scrollPositionRestoration: 'top', // scrolls to top
+    })),
     provideHttpClient(),
     provideAnimationsAsync(),
     // provideClientHydration(withEventReplay()),
